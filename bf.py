fdd9499eb3575d3e9c3d504c62551166a715632e
brick <40585632+a-brick-wall@users.noreply.github.com>
2025-03-17 02:04:12 -0700

    Update bf.py

--- bf.py
+++ bf.py
@@ -1,27 +1,32 @@
+import sys
+sys.path.append('./')
 import brainfuck
 import warnings
 warnings.filterwarnings('ignore')
 warnings.simplefilter('ignore')
 
 i=-1
-bf = ["","+", "-", "<", ">", "[", "]","."]
+bf = ["+", "-", "<", ">", "[", "]","."]
 
 import multiprocessing
 from multiprocessing import Value
 import time
  
 def bf_thread(eval_check,i):
+    test = 1
     while True:
+        test+=1
         i.value=i.value+1
         code = ""
-        for digit in numberToBase(i.value, 8):
+        for digit in numberToBase(i.value, 7):
             code+=bf[digit]
         try:
-            time.sleep(.1)
-            print(brainfuck.evaluate(code))
+            #time.sleep(0.1)
             if brainfuck.evaluate(code)==eval_check:
                 print("the following code evaluates to your desired output")
                 print(code)
+                i.value=-1000
+                break
 
         except:
             pass
@@ -37,18 +42,20 @@ def numberToBase(n, b):
 
 def code_gen(code_len,bf,eval_check):
     i=Value('i',0)
-    while i.value <(8**code_len):
+    end=Value('i',0)
+    while i.value <(7**code_len):
         print(i.value)
-        process = multiprocessing.Process(target=bf_thread, args=(eval_check,i))
+        process = multiprocessing.Process(target=bf_thread, args=(eval_check,i), daemon=True)
         process.start()
         time.sleep(10)
         i=Value('i',i.value)
+        if i.value<-10:
+            sys.exit()
         process.terminate()
         
-    return out
+    return None
 if __name__ == "__main__":
     
-    bf = ["","+", "-", "<", ">", "[", "]","."]
+    bf = ["+", "-", "<", ">", "[", "]","."]
         
-    code_gen(50,bf,"1")
-    x = input()
+    code_gen(5,bf,"=")
